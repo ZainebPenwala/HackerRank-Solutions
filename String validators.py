206c343c4a396d08f70f720724e0198bf1a7c898
Zaineb <zenp1520@gmail.com>
2019-09-06 11:21:38 +0530

    Update String validators.py

--- String validators.py
+++ String validators.py
@@ -1,3 +1,7 @@
+''' ANY() keyword = Returns true if any of the items is True. It returns False if empty or all are false. Any can be thought of as a 
+sequence of OR operations on the provided iterables.It short circuit the execution i.e. stop the execution as soon as the result is 
+known.'''
+
 ## find out if the string contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
 
 s = raw_input()
